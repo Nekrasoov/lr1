690c5fea9b588517e41bde0d36ca2f826ce60e6f
Nekrasoov <derill-mist@yandex.ru>
2024-01-13 23:33:23 +0500

    lr3

--- src/main/java/ru/nekrasov/lr2/util/DateTimeUtil.java
+++ src/main/java/ru/nekrasov/lr2/util/DateTimeUtil.java
@@ -4,6 +4,6 @@ import java.text.SimpleDateFormat;
 
 public class DateTimeUtil {
     public static SimpleDateFormat getCustomFormat() {
-        return new SimpleDateFormat("yyyy-MM-dd`T`HH:mm:ss.SSS`Z`");
+        return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
     }
 }
